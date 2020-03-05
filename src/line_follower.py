12b31e40ba9d52165ec576113975f430a0f593bd
panamass <enzo.gruenberg@mailbox.tu-dresden.de>
2020-03-05 15:34:09 +0100

    Add method to convert RGB values to grayscale

--- src/line_follower.py
+++ src/line_follower.py
@@ -14,3 +14,7 @@ class LineFollower:
         self.us_sensor = us_sensor
 
         self.stop = False
+
+    @staticmethod
+    def rgb_to_grayscale(red, green, blue):
+        return 0.3 * red + 0.59 * green + 0.11 * blue
