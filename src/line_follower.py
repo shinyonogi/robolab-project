c6a450f21193fa664318213c1bde8be713c73f0e
panamass <enzo.gruenberg@mailbox.tu-dresden.de>
2020-03-05 15:32:56 +0100

    Pass hardware instances to LineFollower

--- src/line_follower.py
+++ src/line_follower.py
@@ -3,8 +3,14 @@ class LineFollower:
     Class that controls the robots movements.
     """
 
-    def __init__(self, logger, communication, odometry, planet):
+    def __init__(self, logger, communication, odometry, planet, motor_right, motor_left, color_sensor, us_sensor):
         self.logger = logger
         self.communication = communication
         self.odometry = odometry
         self.planet = planet
+        self.motor_right = motor_right
+        self.motor_left = motor_left
+        self.color_sensor = color_sensor
+        self.us_sensor = us_sensor
+
+        self.stop = False
