96af107e46932bfbc3a2465a2385cdec4168c990
Shintaro Onogi <shintaro.onogi@mailbox.tu-dresden.de>
2020-03-06 03:03:10 +0100

    Odometry improved + test improved

--- src/odometry.py
+++ src/odometry.py
@@ -15,6 +15,7 @@ class Odometry:
         # YOUR CODE FOLLOWS (remove pass, please!)
 
         self.line_of_sight = direction / 57.2958 #angle -> arc
+        self.direction = direction 
         self.coordinate_x = coordinate[0]
         self.coordinate_y = coordinate[1]
 
@@ -22,34 +23,36 @@ class Odometry:
 
         distance_tire = 12
 
+        delta_x = 0
+        delta_y = 0
+
         for i in range(len(motor_stack)):
 
             d_r = Odometry.distance_per_tick(motor_stack[i][0])
             d_l = Odometry.distance_per_tick(motor_stack[i][1])
 
-            delta_x = 0
-            delta_y = 0
-
             angle_alpha = (d_r - d_l) / distance_tire 
+            angle_beta = angle_alpha / 2
 
             if(6.28319 >= angle_alpha >= 6.10865 or 0 <= angle_alpha <= 0.174533 ): #when the way is straight
-                if(self.line_of_sight):
                     distance_s = d_l
-                    delta_x = delta_x + distance_s
-                    delta_y = delta_y + distance_s
+                    if(self.direction == 0): #maybe better to work with arc // precise values better
+                        delta_y = delta_y + distance_s
+                    elif(self.direction== 180):
+                        delta_y = delta_y - distance_s
+                    elif(self.direction == 90):
+                        delta_x = delta_x + distance_s
+                    elif(self.direction == 270):
+                        delta_x = delta_x - distance_s
             else:
-                angle_beta = angle_alpha / 2
                 distance_s = (d_r + d_l)/angle_alpha * math.sin(angle_beta)
                 delta_x = delta_x + -math.sin(self.line_of_sight + angle_beta) * distance_s
                 delta_y = delta_y + math.cos(self.line_of_sight + angle_beta) * distance_s
-
+        
             self.line_of_sight += angle_alpha 
-            self.coordinate_x += delta_x
-            self.coordinate_y += delta_y
 
-
-        self.coordinate_x = round(self.coordinate_x / 50)
-        self.coordinate_y = round(self.coordinate_y / 50)
+        self.coordinate_x = self.coordinate_x + round(delta_x / 50)
+        self.coordinate_y = self.coordinate_y + round(delta_y / 50)
         self.direction = round(self.line_of_sight * 57.2958) % 360
 
         return ((self.coordinate_x, self.coordinate_y), self.direction)
--- src/odometrytest.py
+++ src/odometrytest.py
@@ -2,6 +2,7 @@
 
 from odometry import Odometry
 import unittest
+import math 
 
 class OdometryTest(unittest.TestCase):
     
@@ -17,15 +18,27 @@ class OdometryTest(unittest.TestCase):
 
         odometry = Odometry((0, 0), 0)
         coordinate = odometry.calc_coord(self.motor_stack)
-        self.assertEqual(((1, 1), 0), coordinate)
+        self.assertEqual(((0, 1), 0), coordinate)
 
 
     def test_straight_path_backwards(self):
 
+        odometry = Odometry((0, 1), 180)
+        coordinate = odometry.calc_coord(self.motor_stack)
+        self.assertEqual(((0, 0), 180), coordinate)
+
+    def test_straight_path_east(self):
+
         odometry = Odometry((0, 0), 90)
-        odometry.calc_coord(self.motor_stack)
-        #self.assertEqual((0, 0), (self.odometry.get_cord()))
-        #self.assertEqual
+        coordinate = odometry.calc_coord(self.motor_stack)
+        self.assertEqual(((1, 0), 90), coordinate)
+
+    def test_straight_path_west(self):
+
+        odometry = Odometry((0, 0), 270)
+        coordinate = odometry.calc_coord(self.motor_stack)
+        self.assertEqual(((-1, 0), 270), coordinate)
+
 
 if __name__ == "__main__":
     unittest.main()
