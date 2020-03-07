18e3df17a6e9452a1233605432a3fb607f7ff4f3
panamass <enzo.gruenberg@mailbox.tu-dresden.de>
2020-03-07 11:18:08 +0100

    Started to add color calibration, moved main line follower in method

--- src/explorer.py
+++ src/explorer.py
@@ -1,6 +1,20 @@
 import time
 
 
+def round_ten(number, down=False):
+    """This method rounds down a number to a multiple of ten"""
+    r = int(round(number, -1))
+    if down:
+        return r if r <= number else r - 10
+    else:
+        return r if r >= number else r + 10
+
+
+def rgb_to_grayscale(red, green, blue):
+    """Converts RGB to a greyscale value using luminance"""
+    return 0.3 * red + 0.59 * green + 0.11 * blue
+
+
 class Explorer:
     """
     Class that controls the robots movements.
@@ -18,16 +32,56 @@ class Explorer:
         self.expression = expression
 
         self.target_power = 20
+        self.red_rb_range = ((120, 140), (10, 30))  # TODO: enter defaults (on test planet in E008)
+        self.blue_rb_range = ((30, 50), (80, 110))
 
-        self.stop_cmd = False
-        self.is_running = False
+        self.logger.info("Explorer initialized and ready")
 
-        self.logger.debug("Explorer initialized and ready")
+    def start_exploration(self):
+        self.logger.info("Explorer starting")
+        while True:
+            self.drive_to_next_square()
+        pass
 
-    def start(self):
-        self.logger.debug("Explorer starting")
+    def start_calibration(self):
+        self.logger.info("Starting calibration")
 
-        self.is_running = True
+        for color in ["red", "blue"]:
+            input("Please place the robot's color sensor on a % square, then press enter and slightly move it around on the square for five seconds." % color)
+            cal = self._calibrate()
+            self.logger.debug(cal)
+            rb_range = (
+                    (round_ten(cal[0][0], True), round_ten(cal[0][2])),
+                    (round_ten(cal[2][0], True), round_ten(cal[2][2]))
+                )
+            self.logger.debug(rb_range)
+            if color == "red":
+                self.red_rb_range = rb_range
+            else:
+                self.blue_rb_range = rb_range
+
+        self.logger.info("Calibration done. Exploration can be started now.")
+
+    def _calibrate(self):
+        """Returns a tuple of the following structure:
+        ( (Min Red, Avg Red, Max Red), ... green ... , ... blue ... )
+        For completions sake we include green values, although they're useless af."""
+        self.color_sensor.mode = "RGB-RAW"
+        r = []
+        g = []
+        b = []
+        for i in range(0, 50):
+            rgb = self.color_sensor.bin_data("hhh")
+            r.append(rgb[0])
+            g.append(rgb[1])
+            b.append(rgb[2])
+            time.sleep(0.1)
+        return (min(r), sum(r) / len(r), max(r)), (min(g), sum(g) / len(g), max(g)), (min(b), sum(b) / len(b), max(b))
+
+    def drive_to_next_square(self):
+        blocked = False
+        square_color = None
+        # TODO: expand this as necessary
 
         # Setup hardware
         self.color_sensor.mode = "RGB-RAW"  # Measure RGB values
@@ -40,7 +94,7 @@ class Explorer:
         # See http://www.inpharmix.com/jps/PID_Controller_For_Lego_Mindstorms_Robots.html for documentation
         k_p = 0.11  # Proportional constant
         offset = 170  # Light sensor offset
-        k_i = 0  # Integral constant
+        k_i = 0  # Integral constant, we disable this component because it ruins everything
         integral = 0  # Integral
         k_d = 0.04  # Derivative constant
         last_error = 0
@@ -48,36 +102,35 @@ class Explorer:
         red_counter = 0
         blue_counter = 0
 
-        while not self.stop_cmd:
+        while True:
             if self.us_sensor.distance_centimeters < 15:
+                blocked = True
                 self.logger.debug("Path blocked")
                 self.stop_motors()
-                self.expression.tone_warning()
+                self.expression.tone_warning().wait()
                 self.run_motors(self.target_power, -self.target_power)
                 time.sleep(4)  # Replace with odometry stuff
-                continue
+                continue  # Drive back
 
             rgb = self.color_sensor.bin_data("hhh")  # Read RGB values from sensor
             r = rgb[0]
             g = rgb[1]
             b = rgb[2]
-            gs = self.rgb_to_grayscale(r, g, b)  # Convert RGB to grayscale
+            gs = rgb_to_grayscale(r, g, b)  # Convert RGB to grayscale
 
-            if r > 100 > g and b < 100:
+            r_rb_range = self.red_rb_range
+            b_rb_range = self.blue_rb_range
+            if r_rb_range[0][0] >= r >= r_rb_range[0][1] and r_rb_range[1][0] >= b >= r_rb_range[1][1]:
                 self.logger.debug("Detected RED")
-                # self.expression.beep()  # for testing
+                square_color = "red"
                 self.stop_motors()
-                time.sleep(0.3)
-                self.scan_for_paths()
-            elif 20 <= r <= 40 and 70 <= b <= 100:
+                break
+            elif b_rb_range[0][0] >= r >= b_rb_range[0][1] and b_rb_range[1][0] >= b >= b_rb_range[1][1]:
                 self.logger.debug("Detected BLUE")
                 if blue_counter >= 2:
-                    # self.expression.beep()  # for testing
+                    square_color = "blue"
                     self.stop_motors()
-                    time.sleep(0.5)
-                    self.scan_for_paths()
-                    blue_counter = 0
-                    continue
+                    break
                 else:
                     blue_counter += 1
 
@@ -94,7 +147,7 @@ class Explorer:
 
             time.sleep(0.05)
 
-        self.is_running = False
+        return blocked, square_color
 
     def found_square(self):
         pass
@@ -160,13 +213,5 @@ class Explorer:
         self.color_sensor.mode = "RGB-RAW"
 
     def stop(self):
-        self.stop_cmd = True
-        while self.is_running:
-            time.sleep(0.1)
-        self.stop_cmd = False
         self.stop_motors()
         self.logger.info("Explorer stopped")
-
-    @staticmethod
-    def rgb_to_grayscale(red, green, blue):
-        return 0.3 * red + 0.59 * green + 0.11 * blue
