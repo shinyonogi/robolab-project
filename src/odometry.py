d655d2f9d9ff7b7a2963526bd824a159c4e8b2a4
Panamass <enzo.gruenberg@mailbox.tu-dresden.de>
2020-05-18 20:15:28 +0200

    Comment new odometry

--- src/odometry.py
+++ src/odometry.py
@@ -24,11 +24,14 @@ class Odometry:
         self.wheel_circumference_mm = 2 * math.pi * 28
         self.wheel_distance_mm = 120
 
+        # Current orientation in radians
         self.angle_rad = 0
+        # Position in mm on the 50x50 cm planet grid
         self.x_position_mm = 0
         self.y_position_mm = 0
 
     def set_coord(self, x_coord=None, y_coord=None, direction=None):
+        # Convert coordinates on 50x50 cm planet grid to mm positions
         if x_coord is not None:
             self.x_position_mm = x_coord * 500
 
@@ -51,7 +54,7 @@ class Odometry:
         )
 
     def calc_coord(self):
-        if len(self.motor_stack) == 0:
+        if len(self.motor_stack) == 0:  # Skip calculation if stack is empty
             return self.get_coord()
 
         prev_pos_right = self.motor_stack[0][0]
@@ -59,30 +62,38 @@ class Odometry:
         self.motor_stack.pop(0)
 
         while len(self.motor_stack) > 0:
+            # Get and delete first element from stack
             current_pos = self.motor_stack.pop(0)
             pos_right = current_pos[0]
             pos_left = current_pos[1]
 
+            # Calculate number of ticks (motor degrees) since previous stack element
             ticks_right = pos_right - prev_pos_right
             ticks_left = pos_left - prev_pos_left
 
+            # Skip if robot didn't move
             if not ticks_right and not ticks_left:
                 continue
 
             prev_pos_right = pos_right
             prev_pos_left = pos_left
 
+            # Number of motor rotations
             rotations_right = float(ticks_right / 360)
             rotations_left = float(ticks_left / 360)
 
+            # Distance moved in mm on both wheels
             mm_right = float(rotations_right * self.wheel_circumference_mm)
             mm_left = float(rotations_left * self.wheel_circumference_mm)
 
+            # Distance moved in mm by the robot
             mm = (mm_right + mm_left) / 2.0
 
+            # Calculate new orientation and limit it to 0 to 2*Pi (0 to 360 degrees)
             self.angle_rad += (mm_left - mm_right) / self.wheel_circumference_mm
             self.angle_rad %= 2 * math.pi
 
+            # Calculate new x and y positions
             self.x_position_mm += mm * math.sin(self.angle_rad)
             self.y_position_mm += mm * math.cos(self.angle_rad)
 
